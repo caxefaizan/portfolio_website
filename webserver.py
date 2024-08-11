05712909cf5b3ef1a171da84da6b4e1913966307
faizan.qazi <faizanzahid09@hotmail.com>
2024-08-11 16:28:14 +0530

    enable email

--- webserver.py
+++ webserver.py
@@ -1,4 +1,11 @@
-from flask import Flask, render_template, request, redirect, send_from_directory
+from flask import (
+    Flask,
+    render_template,
+    request,
+    send_from_directory,
+    redirect,
+    url_for,
+)
 import csv, json
 from datetime import date
 from flask_mail import Mail, Message
@@ -6,7 +13,7 @@ from flask_mail import Mail, Message
 app = Flask(__name__)
 
 with open("./profiledata.json", "r") as fp:
-    data = json.loads(fp.read())
+    profile_data = json.loads(fp.read())
 with open("./config.txt", "r") as fp:
     conf = fp.read()
 
@@ -23,29 +30,22 @@ mail = Mail(app)
 # route to serve the first .exe file
 @app.route("/download/resume")
 def download_file():
-    print(f'{data["basic"]["name"].lower().replace(" ","")}')
     return send_from_directory(
         directory="./static/assets/downloads",
-        path=f'{data["basic"]["name"].lower().replace(" ","")}-resume.pdf',
+        path=f'{profile_data["basic"]["name"].lower().replace(" ","")}-resume.pdf',
         as_attachment=True,
     )
 
 
-@app.route(
-    "/"
-)  # for loading templates, default folder it looks from and necesary is templates also for css,js,favicon those should be in static folder
+@app.route("/")
 def webpage():
-    return render_template("index.html", data=data)
-
-
-@app.route(
-    "/2"
-)  # for loading templates, default folder it looks from and necesary is templates also for css,js,favicon those should be in static folder
-def webpage2():
-    return render_template("indexs.html", data=data)
+    return render_template("index.html", data=profile_data)
 
 
 def write_to_csv(data):
+    email = data["email"]
+    name = data["name"]
+    message = data["message"]
     try:
         msg = Message(
             f"{name}", sender=user_id, recipients=["faizanzahid09@hotmail.com"]
@@ -54,9 +54,6 @@ def write_to_csv(data):
         mail.send(msg)
     except:
         with open("./database.csv", newline="", mode="a") as database:
-            email = data["email"]
-            name = data["name"]
-            message = data["message"]
             csv_writer = csv.writer(
                 database, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL
             )
@@ -69,8 +66,12 @@ def submit_form():
         try:
             data = request.form.to_dict()
             write_to_csv(data)
-            return redirect("/thankyou.html")
         except Exception as e:
             return str(e) + "did not save in database"
     else:
         print("Something wrong")
+    return redirect(url_for("webpage"))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
